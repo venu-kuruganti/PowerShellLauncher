7c879b04a1666d491352042e941592b15c6c2689
Venu Kuruganti <35195194+venu-kuruganti@users.noreply.github.com>
2022-06-24 20:40:20 +0530

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,53 @@
+﻿using System.Management.Automation;
+using System.Management.Automation.Runspaces;
+using System.Configuration;
+using Windows.Storage;
+
+string GameName = ApplicationData.Current.LocalSettings.Values["gamename"] as string;
+string path = ConfigurationSettings.AppSettings["MameDirectoryLocation"].ToString().Trim();
+
+//create runspace
+Runspace runspace = RunspaceFactory.CreateRunspace();
+
+//Open it
+runspace.Open();
+
+//Create a pipeline
+
+Pipeline pipeline = runspace.CreatePipeline();
+pipeline.Commands.AddScript("Set-ExecutionPolicy -Scope Process -ExecutionPolicy Unrestricted");
+pipeline.Commands.AddScript("$myCommand = \"cd " + path + "\"");
+pipeline.Commands.AddScript("iex $myCommand");
+pipeline.Commands.AddScript("$runGameCommand = \"./mame.exe \'" + GameName + "' -plugin autofire\"");
+pipeline.Commands.AddScript("iex $runGameCommand");        
+
+
+try
+{
+    pipeline.Invoke();
+}
+catch (Exception ex)
+{
+    Console.WriteLine(ex.Message);
+    Console.ReadKey();
+
+}
+
+
+//Close runspace
+runspace.Close();
+
+
+/*
+ *$myCommand = "cd C:\Users\venuk\Documents\Personal\Games\MAME"
+iex $myCommand
+
+$runGameCommand = "./mame.exe '" + $args[0] + "' -plugin autofire"
+iex $runGameCommand
+ 
+ */
+
+
+
+
+
